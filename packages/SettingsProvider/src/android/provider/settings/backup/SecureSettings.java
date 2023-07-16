29096714ae9c877d41b0db6a00f95590b572157d
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-07-16 11:29:25 +0530

    Update SecureSettings.java

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -225,5 +225,9 @@ public class SecureSettings {
         Settings.Secure.LOCK_SCREEN_WEATHER_ENABLED,
         Settings.Secure.ACCESSIBILITY_FONT_SCALING_HAS_BEEN_CHANGED,
         Settings.Secure.SHOW_WIFI_STANDARD_ICON,
+        Settings.Secure.DOZE_FOR_NOTIFICATIONS,
+        Settings.Secure.DOZE_SINGLE_TAP_GESTURE_AMBIENT,
+        Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_AMBIENT,
+        Settings.Secure.DOZE_PICK_UP_GESTURE_AMBIENT,
     };
 }
