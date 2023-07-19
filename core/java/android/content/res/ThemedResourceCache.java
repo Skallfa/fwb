32fc932b5aa7a186f8d1135a6f4ca9088586a989
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-07-19 20:34:17 +0530

    Update ThemedResourceCache.java

--- core/java/android/content/res/ThemedResourceCache.java
+++ core/java/android/content/res/ThemedResourceCache.java
@@ -22,9 +22,9 @@ import android.compat.annotation.UnsupportedAppUsage;
 import android.content.pm.ActivityInfo.Config;
 import android.content.res.Resources.Theme;
 import android.content.res.Resources.ThemeKey;
-import android.util.ArrayMap;
 import android.util.LongSparseArray;
 
+import java.util.HashMap;
 import java.lang.ref.WeakReference;
 
 /**
@@ -34,7 +34,7 @@ import java.lang.ref.WeakReference;
  */
 abstract class ThemedResourceCache<T> {
     @UnsupportedAppUsage
-    private ArrayMap<ThemeKey, LongSparseArray<WeakReference<T>>> mThemedEntries;
+    private HashMap<ThemeKey, LongSparseArray<WeakReference<T>>> mThemedEntries;
     private LongSparseArray<WeakReference<T>> mUnthemedEntries;
     private LongSparseArray<WeakReference<T>> mNullThemedEntries;
 
@@ -154,7 +154,7 @@ abstract class ThemedResourceCache<T> {
 
         if (mThemedEntries == null) {
             if (create) {
-                mThemedEntries = new ArrayMap<>(1);
+                mThemedEntries = new HashMap<>(1);
             } else {
                 return null;
             }
@@ -198,12 +198,8 @@ abstract class ThemedResourceCache<T> {
      */
     private boolean prune(@Config int configChanges) {
         synchronized (this) {
-            if (mThemedEntries != null) {
-                for (int i = mThemedEntries.size() - 1; i >= 0; i--) {
-                    if (pruneEntriesLocked(mThemedEntries.valueAt(i), configChanges)) {
-                        mThemedEntries.removeAt(i);
-                    }
-                }
+            mThemedEntries.entrySet()
+                    .removeIf(entry -> pruneEntriesLocked(entry.getValue(), configChanges));
             }
 
             pruneEntriesLocked(mNullThemedEntries, configChanges);
@@ -212,7 +208,6 @@ abstract class ThemedResourceCache<T> {
             return mThemedEntries == null && mNullThemedEntries == null
                     && mUnthemedEntries == null;
         }
-    }
 
     private boolean pruneEntriesLocked(@Nullable LongSparseArray<WeakReference<T>> entries,
             @Config int configChanges) {
