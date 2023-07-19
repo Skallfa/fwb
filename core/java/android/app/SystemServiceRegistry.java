b32cae958134d13b7b3c138c8f2534067a9652d3
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-07-19 19:04:22 +0530

    Update SystemServiceRegistry.java

--- core/java/android/app/SystemServiceRegistry.java
+++ core/java/android/app/SystemServiceRegistry.java
@@ -195,8 +195,6 @@ import android.permission.LegacyPermissionManager;
 import android.permission.PermissionCheckerManager;
 import android.permission.PermissionControllerManager;
 import android.permission.PermissionManager;
-import android.pocket.IPocketService;
-import android.pocket.PocketManager;
 import android.print.IPrintManager;
 import android.print.PrintManager;
 import android.safetycenter.SafetyCenterFrameworkInitializer;
@@ -974,15 +972,6 @@ public final class SystemServiceRegistry {
                 ILinearmotorVibratorService service = ILinearmotorVibratorService.Stub.asInterface(binder);
                 return new LinearmotorVibrator(ctx.getOuterContext(), service);
             }});
-
-        registerService(Context.POCKET_SERVICE, PocketManager.class,
-                new CachedServiceFetcher<PocketManager>() {
-                    @Override
-                    public PocketManager createService(ContextImpl ctx) {
-                        IBinder binder = ServiceManager.getService(Context.POCKET_SERVICE);
-                        IPocketService service = IPocketService.Stub.asInterface(binder);
-                        return new PocketManager(ctx.getOuterContext(), service);
-                    }});
 
         registerService(Context.TV_INPUT_SERVICE, TvInputManager.class,
                 new CachedServiceFetcher<TvInputManager>() {
