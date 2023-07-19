730ee9cac1d176e131afb926e084c2900642cdb8
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-07-19 19:24:09 +0530

    Update SystemServer.java

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1688,9 +1688,7 @@ public final class SystemServer implements Dumpable {
 
         // Before things start rolling, be sure we have decided whether
         // we are in safe mode.
-        if(wm != null) {
-            safeMode = wm.detectSafeMode();
-        }
+        final boolean safeMode = wm.detectSafeMode();
         if (safeMode) {
             // If yes, immediately turn on the global setting for airplane mode.
             // Note that this does not send broadcasts at this stage because
@@ -2523,10 +2521,6 @@ public final class SystemServer implements Dumpable {
             mSystemServiceManager.startService(CrossProfileAppsService.class);
             t.traceEnd();
 
-            t.traceBegin("StartPocketService");
-            mSystemServiceManager.startService(PocketService.class);
-            t.traceEnd();
-
             t.traceBegin("StartPeopleService");
             mSystemServiceManager.startService(PeopleService.class);
             t.traceEnd();
@@ -2534,13 +2528,6 @@ public final class SystemServer implements Dumpable {
             t.traceBegin("StartMediaMetricsManager");
             mSystemServiceManager.startService(MediaMetricsManagerService.class);
             t.traceEnd();
-
-             if (!context.getResources().getString(
-                    com.android.internal.R.string.config_pocketBridgeSysfsInpocket).isEmpty()) {
-                t.traceBegin("StartPocketBridgeService");
-                mSystemServiceManager.startService(PocketBridgeService.class);
-                t.traceEnd();
-            }
 
             t.traceBegin("StartLinearmotorVibratorService");
             mSystemServiceManager.startService(LinearmotorVibratorService.class);
