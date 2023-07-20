cc540c79f66716e0b632d89ef061b85b94ff5387
Skallfa <128469350+Skallfa@users.noreply.github.com>
2023-07-20 09:09:17 +0530

    Update CollapsedStatusBarFragment.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -521,6 +521,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             if (!isRightClock) {
                 state |= DISABLE_CLOCK;
             }
+                state |= DISABLE_NOTIFICATION_ICONS;
         }
 
         if (mOngoingCallController.hasOngoingCall()) {
