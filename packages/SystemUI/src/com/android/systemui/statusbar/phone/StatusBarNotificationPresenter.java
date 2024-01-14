07d88ee70effd37cf5cfb5a09f2dc5923cebe38c
Skallfa <128469350+Skallfa@users.noreply.github.com>
2024-01-14 17:52:18 +0530

    Update StatusBarNotificationPresenter.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarNotificationPresenter.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarNotificationPresenter.java
@@ -388,7 +388,7 @@ class StatusBarNotificationPresenter implements NotificationPresenter,
 
         @Override
         public boolean suppressAwakeInterruptions(NotificationEntry entry) {
-            return isDeviceInVrMode() || entry.getSbn().getIsContentSecure();
+            return isDeviceInVrMode();
         }
 
         @Override
