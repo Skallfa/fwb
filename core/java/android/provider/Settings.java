f801a7b37c4a78130c854df0812e77533cd7b4cb
Skallfa <128469350+Skallfa@users.noreply.github.com>
2024-02-14 18:21:34 +0530

    Update Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11777,12 +11777,6 @@ public final class Settings {
         public static final String PULSE_TRIGGER_REASON = "pulse_trigger_reason";
 
         /**
-         * Whether to pulse ambient on new music tracks
-         * @hide
-         */
-        public static final String PULSE_ON_NEW_TRACKS = "pulse_on_new_tracks";
-
-        /**
          * Pulse navbar music visualizer
          * @hide
          */
